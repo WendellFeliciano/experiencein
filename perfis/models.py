9e3cd11557330bcf161ac52cf13a7e9132d481a4
Wendell Feliciano <wrf1974@gmail.com>
2021-03-18 16:26:57 -0300

    carga da cópia do projeto local

---
+++ perfis/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Perfil(models.Model):
+    nome = models.CharField(max_length=255, null=False, default='')
+    #email = models.CharField(max_length=255, null=False, default='')   #já existe em user  
+    telefone = models.CharField(max_length=15, null=False, default='')
+    nome_empresa = models.CharField(max_length=255, null=False, default='')
+    contatos = models.ManyToManyField('self')
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE, related_name="perfil")
+    @property
+    def email(self):
+        return self.usuario.email
+    
+    def convidar(self, perfil_convidado):
+        convite = Convite(solicitante=self, convidado=perfil_convidado)
+        convite.save()
+
+class Convite(models.Model):
+    solicitante = models.ForeignKey(Perfil, on_delete=models.CASCADE, related_name='convites_feitos')
+    convidado = models.ForeignKey(Perfil, on_delete=models.CASCADE, related_name='convites_recebidos')
+
+    def aceitar(self):
+        self.convidado.contatos.add(self.solicitante)
+        self.solicitante.contatos.add(self.convidado)
+        self.delete()
